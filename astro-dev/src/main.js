a0a8642062aa38233c9fdfde570a678a5c278e53
Alekiel42 <alexandra.bercoff@gmail.com>
2021-10-27 16:18:22 +0200

    config font awesome

--- astro-dev/src/main.js
+++ astro-dev/src/main.js
@@ -2,4 +2,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
 
-createApp(App).mount('#app')
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faPhone } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faPhone);
+
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+createApp(App)
+    .component('ficon', FontAwesomeIcon)
+    .mount('#app')
