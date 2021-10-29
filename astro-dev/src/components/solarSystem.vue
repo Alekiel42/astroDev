06406fe1908cb531009b3ddf4236e99da9b2f490
Alekiel42 <alexandra.bercoff@gmail.com>
2021-10-29 17:59:23 +0200

    color spaceObject ok

--- astro-dev/src/components/solarSystem.vue
+++ astro-dev/src/components/solarSystem.vue
@@ -1,14 +1,17 @@
 <template>
-  <div class="flex">
+  <div class="flex mb-8">
     <div
       class="flex-initial"
       v-for="(spaceObject, index) in systemSolar"
       :key="index"
     >
-      <div
-        class="w-20 h-20 m-2 rounded-full"
-        v-bind:style="{ backgroundColor: spaceObject.color }"
-      ></div>
+      <!-- todo component planet-->
+      <div>
+        <div
+          class="w-14 h-14 m-6 rounded-full"
+          v-bind:style="{ backgroundColor: spaceObject.color }"
+        ></div>
+      </div>
       <p>{{ spaceObject.name }}</p>
     </div>
   </div>
@@ -21,47 +24,49 @@ export default {
       systemSolar: [
         {
           name: "Sun",
-          color: "ffdb4d",
+          color: "#ffdb4d",
         },
         {
           name: "Mercury",
-          color: "b08d02",
+          color: "#b08d02",
         },
         {
           name: "Venus",
-          color: "db8b00",
+          color: "#db8b00",
         },
         {
           name: "Earth",
-          color: "338ca2",
+          color: "#338ca2",
         },
         {
           name: "Mars",
-          color: "990800",
+          color: "#990800",
         },
         {
           name: "Jupiter",
-          color: "ffe3a3",
+          color: "#ffe3a3",
         },
         {
           name: "Saturne",
-          color: "d9b3ff",
+          color: "#d9b3ff",
         },
         {
           name: "Uranus",
-          color: "b3f7ff",
+          color: "#b3f7ff",
         },
         {
           name: "Neptune",
-          color: "04029e",
+          color: "#04029e",
         },
         {
           name: "Pluto",
-          color: "403301",
+          color: "#403301",
         },
       ],
     };
   },
-  methods: {},
+  methods: {
+    selectSpaceObject(name) {},
+  },
 };
 </script>
