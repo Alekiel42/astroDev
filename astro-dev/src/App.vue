da229aaa7aded89bfbd08b520688f129f0e4d56a
Alekiel42 <alexandra.bercoff@gmail.com>
2021-10-30 13:56:25 +0200

    show planet and star information + toggle visibiliy

--- astro-dev/src/App.vue
+++ astro-dev/src/App.vue
@@ -1,12 +1,13 @@
 <template>
   <section class="text-white" id="app">
-    <solarSystem />
     <div>
       <h1 class="uppercase text-4xl font-bold mb-2">Astro Dev</h1>
       <p class="italic mb-4">
         when I was young I wanted to be an astrophysicist
       </p>
     </div>
+
+    <solarSystem />
 
     <picOfDay @add-picture-liked="addFavoritePicture" />
 
--- astro-dev/src/components/solarSystem.vue
+++ astro-dev/src/components/solarSystem.vue
@@ -12,6 +12,12 @@
       />
     </div>
   </div>
+  <button class="bg-gray-400 p-2 m-2" @click="toggleInformationVisibility">
+    Show / Hide informations
+  </button>
+  <div v-if="informationIsVisible">
+    <p>{{ informationToShow }}</p>
+  </div>
 </template>
 
 <script>
@@ -23,55 +29,69 @@ export default {
   },
   data() {
     return {
+      informationIsVisible: false,
+      informationToShow: "",
       systemSolar: [
         {
           name: "Sun",
           color: "#ffdb4d",
+          information:
+            "The Sun is the star at the center of the Solar System. It is a nearly perfect ball of hot plasma heated to incandescence by nuclear fusion reactions in its core, radiating the energy mainly as visible light, ultraviolet light, and infrared radiation. ",
           selected: false,
         },
         {
           name: "Mercury",
           color: "#b08d02",
+          information:
+            "Mercury is the smallest planet in the Solar System and the closest to the Sun. Its orbit around the Sun takes 87.97 Earth days, the shortest of all the Sun's planets.",
           selected: false,
         },
         {
           name: "Venus",
           color: "#db8b00",
+          information: "",
           selected: false,
         },
         {
           name: "Earth",
           color: "#338ca2",
+          information: "",
           selected: false,
         },
         {
           name: "Mars",
           color: "#990800",
+          information: "",
           selected: false,
         },
         {
           name: "Jupiter",
           color: "#ffe3a3",
+          information: "",
           selected: false,
         },
         {
           name: "Saturne",
           color: "#d9b3ff",
+          information: "",
           selected: false,
         },
         {
           name: "Uranus",
           color: "#b3f7ff",
+          information: "",
           selected: false,
         },
         {
           name: "Neptune",
           color: "#04029e",
+          information: "",
           selected: false,
         },
         {
           name: "Pluto",
           color: "#403301",
+          information: "",
           selected: false,
         },
       ],
@@ -80,13 +100,17 @@ export default {
   computed: {},
   methods: {
     setNewObjectSelected(name) {
-      console.log("ok", name);
-      // enlever selected a tous
       this.systemSolar.forEach((elem) => {
         name === elem.name ? (elem.selected = true) : (elem.selected = false);
       });
-      console.log("hh", this.systemSolar);
-      //! pas rerender si true computed
+
+      const objectSelected = this.systemSolar.find(
+        (elem) => elem.name === name
+      );
+      this.informationToShow = objectSelected.information;
+    },
+    toggleInformationVisibility() {
+      this.informationIsVisible = !this.informationIsVisible;
     },
   },
 };
