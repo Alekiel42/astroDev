145ca20531dba27968d0fb0f51ab29f5be827be1
Alekiel42 <alexandra.bercoff@gmail.com>
2021-10-30 16:15:23 +0200

    improve css modal

--- astro-dev/src/components/picDetail.vue
+++ astro-dev/src/components/picDetail.vue
@@ -1,15 +1,32 @@
 <template>
-  <div class="absolute top-10 left-10 z-10">
-    <div class="bg-gray-900 p-10 w-5/6 max-w-md">
-      <!-- todo alt -->
+  <div class="absolute top-0 left-0 z-10">
+    <div
+      class="
+        bg-gray-900 bg-opacity-25
+        backdrop-filter backdrop-blur-md
+        p-10
+        fixed
+        h-screen
+      "
+    >
       <button
-        class="p-1 m-3 rounded-sm bg-indigo-600 text-black"
+        class="absolute p-2 m-6 rounded-sm bg-indigo-600 text-gray-200 right-10"
         @click="closeModal"
       >
         Close
       </button>
-      <img class="object-cover rounded-2xl" v-bind:src="info.hdurl" alt="" />
-      <p class="mt-4 text-xs">{{ info.explanation }}</p>
+      <div class="bg-gray-800 p-10 rounded-lg">
+        <!-- todo alt -->
+
+        <div class="flex">
+          <img
+            class="object-cover rounded-2xl w-1/4 mr-6"
+            v-bind:src="info.hdurl"
+            alt=""
+          />
+          <p class="mt-4 mr-24 ml-10 text-md">{{ info.explanation }}</p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
