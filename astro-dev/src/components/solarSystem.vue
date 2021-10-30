7428f4e3a26ff46ac69e6fc700d3926b8c416ded
Alekiel42 <alexandra.bercoff@gmail.com>
2021-10-30 13:40:50 +0200

    select space object

--- astro-dev/src/components/solarSystem.vue
+++ astro-dev/src/components/solarSystem.vue
@@ -5,68 +5,89 @@
       v-for="(spaceObject, index) in systemSolar"
       :key="index"
     >
-      <!-- todo component planet-->
-      <div>
-        <div
-          class="w-14 h-14 m-6 rounded-full"
-          v-bind:style="{ backgroundColor: spaceObject.color }"
-        ></div>
-      </div>
-      <p>{{ spaceObject.name }}</p>
+      <systemSolarObject
+        :object="spaceObject"
+        :isSelected="spaceObject.selected"
+        @change-object-selected="setNewObjectSelected"
+      />
     </div>
   </div>
 </template>
 
 <script>
+import systemSolarObject from "./spaceObject.vue";
+
 export default {
+  components: {
+    systemSolarObject,
+  },
   data() {
     return {
       systemSolar: [
         {
           name: "Sun",
           color: "#ffdb4d",
+          selected: false,
         },
         {
           name: "Mercury",
           color: "#b08d02",
+          selected: false,
         },
         {
           name: "Venus",
           color: "#db8b00",
+          selected: false,
         },
         {
           name: "Earth",
           color: "#338ca2",
+          selected: false,
         },
         {
           name: "Mars",
           color: "#990800",
+          selected: false,
         },
         {
           name: "Jupiter",
           color: "#ffe3a3",
+          selected: false,
         },
         {
           name: "Saturne",
           color: "#d9b3ff",
+          selected: false,
         },
         {
           name: "Uranus",
           color: "#b3f7ff",
+          selected: false,
         },
         {
           name: "Neptune",
           color: "#04029e",
+          selected: false,
         },
         {
           name: "Pluto",
           color: "#403301",
+          selected: false,
         },
       ],
     };
   },
+  computed: {},
   methods: {
-    selectSpaceObject(name) {},
+    setNewObjectSelected(name) {
+      console.log("ok", name);
+      // enlever selected a tous
+      this.systemSolar.forEach((elem) => {
+        name === elem.name ? (elem.selected = true) : (elem.selected = false);
+      });
+      console.log("hh", this.systemSolar);
+      //! pas rerender si true computed
+    },
   },
 };
 </script>
---
+++ astro-dev/src/components/spaceObject.vue
@@ -0,0 +1,38 @@
+<template>
+  <div
+    class="rounded-full border-solid border-4 border-white m-2"
+    :class="isSelected ? 'border-opacity-100' : 'border-opacity-0'"
+    @click="selectObject"
+  >
+    <div
+      class="w-14 h-14 m-2 rounded-full"
+      v-bind:style="{ backgroundColor: object.color }"
+    ></div>
+  </div>
+  <p>{{ object.name }}</p>
+</template>
+
+<script>
+export default {
+  emits: ["change-object-selected"],
+  props: {
+    object: {
+      type: Object,
+      default: () => {},
+    },
+    isSelected: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {
+    selectObject() {
+      this.$emit("change-object-selected", this.object.name);
+    },
+  },
+};
+</script>
