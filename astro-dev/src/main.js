dfb3b8244b9d6deaa2ed22a4f0ad562a785ef47a
Alekiel42 <alexandra.bercoff@gmail.com>
2021-11-20 13:50:40 +0100

    add BaseDialog to asyncComponent

--- astro-dev/src/main.js
+++ astro-dev/src/main.js
@@ -1,16 +1,16 @@
-import { createApp } from 'vue'
+import './index.css'
+import { createApp, defineAsyncComponent } from 'vue'
+
 import App from './App.vue'
 import BaseButton from './components/UI/BaseButton.vue';
 import BaseCard from './components/UI/BaseCard.vue';
-import BaseDialog from './components/UI/BaseDialog.vue';
-import './index.css'
+const BaseDialog = defineAsyncComponent(() => import('./components/UI/BaseDialog.vue'));
 
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faTrash, faSpaceShuttle } from "@fortawesome/free-solid-svg-icons";
-
 library.add(faTrash, faSpaceShuttle);
-
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
 
 const app = createApp(App);
 
